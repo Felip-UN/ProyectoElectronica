46ddc8da3cbde65937426c8de96ca7778cc2c41f
Juan3301 <juleonm@unal.edu.co>
2025-03-03 06:26:08 -0500

    Update Sensor MQ2.ino

--- Sensor MQ2.ino
+++ Sensor MQ2.ino
@@ -1,18 +1,27 @@
-int gas_detectado = 0;  // Variable para guardar la señal
+/* Change the threshold value with your own reading */
+#define Threshold 400
+
+#define MQ2pin 0
+
+float sensorValue;  //variable to store sensor value
 
 void setup() {
-    pinMode(SENSOR_PIN, INPUT);
-    Serial.begin(115200);
+	Serial.begin(9600); // sets the serial port to 9600
+	Serial.println("MQ2 warming up!");
+	delay(20000); // allow the MQ2 to warm up
 }
 
 void loop() {
-    gas_detectado = digitalRead(SENSOR_PIN);  // Leer la señal del sensor
+  sensorValue = analogRead(MQ2pin); // read analog input pin 0
+  
+  Serial.print("Sensor Value: ");
+  Serial.print(sensorValue);
 
-    if (gas_detectado == HIGH) {  // Si hay gas metano
-        Serial.println("Gas detectado, señal = 1");
-    } else {
-        Serial.println("Sin gas, señal = 0");
-    }
-
-    delay(600);  // Pequeño retraso para evitar lecturas constantes
+  if(sensorValue > Threshold)
+  {
+    Serial.print(" | Smoke detected!");
+  }
+  
+  Serial.println("");
+  delay(2000); // wait 2s for next reading
 }
