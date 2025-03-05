4b211712bab6098600ebe5116bff6f7a940efc36
Juan3301 <juleonm@unal.edu.co>
2025-03-05 13:04:01 -0500

    Update Sensor de humedad.ino

--- Sensor de humedad.ino
+++ Sensor de humedad.ino
@@ -1,18 +1,20 @@
 void setup() {
-    //Paso 1
+    //Paso 1 Se establecen los 9300 baudios del monitor serie y un mensaje de inicio de lectura 
     Serial.begin(9600);
-    Serial.println("Valor del sensor de humedad");
+    Serial.println('Sensor de humedad');
 }
   
 void loop() {
-    //Paso 2
+    //Paso 2 Lee el valor del sensor en el pin A0 y lo almacena en la variable humidity
     int humidity = analogRead(A0);
-    Serial.print("Lectura:");
+    Serial.print("Lectura:"); //Muestra el valor de la humedad en el monitor serie.
     Serial.println(humidity);
-    //Paso 3
-    if(humidity >= 0 & humidity <= 300){
+
+    
+    //Paso 3 Se comparan los valores de humedad y se establecen umbrales para suelo seco, humeda y agua. 
+    if(humidity >= 0 && humidity <= 300){
         Serial.println("Sensor en suelo seco");  
-    } else if(humidity > 301 & humidity <= 700){
+    } else if(humidity > 301 && humidity <= 700){
         Serial.println("Sensor en suelo húmedo");
     }else if(humidity >= 701){
         Serial.println("Sensor en agua");
