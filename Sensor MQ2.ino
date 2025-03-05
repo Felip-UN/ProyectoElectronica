cac3d6868cabbf6fc8b0225f9d254fee84b8f2a2
Juan3301 <juleonm@unal.edu.co>
2025-03-05 12:57:16 -0500

    Update Sensor MQ2.ino

--- Sensor MQ2.ino
+++ Sensor MQ2.ino
@@ -1,25 +1,28 @@
 /* Change the threshold value with your own reading */
-#define Threshold 400
+#define Threshold 400  
+// Es el umbral (400) que determina si hay presencia de humo/gas.
 
 #define MQ2pin 0
+// Define que el sensor está conectado al pin analógico A0 (pin 0).
 
-float sensorValue;  //variable to store sensor value
+float sensorValue;  //variable para almacenar el valor del sensor
 
 void setup() {
-	Serial.begin(9600); // sets the serial port to 9600
-	Serial.println("MQ2 warming up!");
-	delay(20000); // allow the MQ2 to warm up
+	Serial.begin(9600); //Establecer el puerto serie en 9600
+	Serial.println("MQ2 warming up!"); //Se muestra un mensaje "MQ2 warming up!" porque el sensor necesita 20 segundos (delay(20000)) para calentarse antes de funcionar correctamente.
+	delay(20000); // Tiempo de espera para que el sensor pueda calentarse 
 }
 
 void loop() {
-  sensorValue = analogRead(MQ2pin); // read analog input pin 0
+  sensorValue = analogRead(MQ2pin); //Lee el pin de la entrada que se asignó anteriormente A0
   
   Serial.print("Sensor Value: ");
-  Serial.print(sensorValue);
+  Serial.print(sensorValue); //Se imprime el valor leído por el sensor
 
   if(sensorValue > Threshold)
   {
-    Serial.print(" | Smoke detected!");
+    Serial.print(" | Smoke detected!"); 
+// Si el valor del sensor es mayor al umbral (400), significa que se ha detectado humo y se muestra el mensaje "Smoke detected!".
   }
   
   Serial.println("");
